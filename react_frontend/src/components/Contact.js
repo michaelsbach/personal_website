fc270c1b26ffdbe368d47e8209200726f7816816
michaelsbach <m.sbach24@gmail.com>
2020-03-11 16:26:37 -0500

    simplifying information components

--- react_frontend/src/components/Contact.js
+++ react_frontend/src/components/Contact.js
@@ -1,22 +1,25 @@
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 import React from 'react';
-import Container from 'react-bootstrap/Container'
 import './components.css';
 
 
 function Contact(){
 return(
+    <div className="section">
+        <h1 className="header"> Contact </h1>
+        <div className="section-content">
+            <h5> Email: m.sbach24@gmail.com</h5>
+            <h5> icons & links to github & linkedin</h5>
+        </div>
+        <h1 className="header"> About Me </h1>
+        <div className="section-content">
+            <h5> Hello! I'm Michael, a recent grad based in Central Texas with a passion for challenges and solving problems using technology. 
+                    I started learning to code about a year ago and found loving the process that went into writing even the smallest programs. 
+                    I strive to always grow and improve, and learn as much as I can.
 
-<Container>
-    <Row>
-        <p> Email: m.sbach24@gmail.com</p>
-    </Row>
-    <Row>
-        <p> icons & links to github & linkedin</p>
-    </Row>
-</Container>
-
+                    I am working on developing applications that provide intuitive user interfaces paired with efficient backend architecture. 
+            </h5>
+        </div>
+    </div>
 );
 }
 
