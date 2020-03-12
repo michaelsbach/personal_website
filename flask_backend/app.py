6076df6af40c6e85fa9dbf5a7f8cbfae01324886
Michael Schwarzbach <m.sbach24@gmail.com>
2020-03-12 21:46:42 +0000

    modifying app.py and adding wsgi.py for gunicorn usage

--- flask_backend/app.py
+++ flask_backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 import json, yaml, multiprocessing
-import gunicorn.app.base
-from gunicorn.six import iteritems
+#import gunicorn.app.base
+#from gunicorn.six import iteritems
 
 def number_of_workers():
     return (multiprocessing.cpu_count() * 2) + 1
@@ -26,29 +26,32 @@ def api_projects():
         except yaml.YAMLError as err:
             return json.dumps({"ERROR": err}), 500
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
+
 
 # new
-class StandaloneApplication(gunicorn.app.base.BaseApplication):
-
-    def __init__(self, app, options=None):
-        self.options = options or {}
-        self.application = app
-        super(StandaloneApplication, self).__init__()
-
-    def load_config(self):
-        config = dict([(key, value) for key, value in iteritems(self.options)
-                       if key in self.cfg.settings and value is not None])
-        for key, value in iteritems(config):
-            self.cfg.set(key.lower(), value)
-
-    def load(self):
-        return self.application
-
-
-if __name__ == '__main__':
-    options = {
-        'bind': '%s:%s' % ('127.0.0.1', '8080'),
-        'workers': number_of_workers(),
-    }
-    # Modification 3: pass Flask app instead of handler_app
-    StandaloneApplication(app, options).run()
+#class StandaloneApplication(gunicorn.app.base.BaseApplication):
+#
+#    def __init__(self, app, options=None):
+#        self.options = options or {}
+#        self.application = app
+#        super(StandaloneApplication, self).__init__()
+#
+#    def load_config(self):
+#        config = dict([(key, value) for key, value in iteritems(self.options)
+#                       if key in self.cfg.settings and value is not None])
+#        for key, value in iteritems(config):
+#            self.cfg.set(key.lower(), value)
+#
+#    def load(self):
+#        return self.application
+#
+#
+#if __name__ == '__main__':
+#    options = {
+#        'bind': '%s:%s' % ('127.0.0.1', '8080'),
+#        'workers': number_of_workers(),
+#    }
+#    # Modification 3: pass Flask app instead of handler_app
+#    StandaloneApplication(app, options).run()
