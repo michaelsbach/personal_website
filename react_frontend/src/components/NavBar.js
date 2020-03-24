a6f90b3d1f5b9c4ecb865345bb2a400b7bff47c1
michaelsbach <m.sbach24@gmail.com>
2020-03-24 14:07:50 -0500

    implementing react-scroll; adding scroll listener to shrink navbar when scrolling; modularizing Links

--- react_frontend/src/components/NavBar.js
+++ react_frontend/src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
@@ -7,29 +7,67 @@ import './components.css';
 
 
 
-function NavBar(){
-return (
+function NavBar(props){
     
-<Container fluid id="navbar">
-    <Row className="justify-content-center py-2"> 
-        <Col xs={5} className="text-left">
-            <h4><a href="/" className="navLink" >Home</a></h4>
-        </Col>
+    let scrollToTop = () => scroll.scrollToTop();
+    
+    const [scrolled, setScrolled] = useState(false);
 
-        <Col xs={3}>
-            <Link to="contact" smooth={true} duration= {500}>
-                <h4 className="navLink">Contact</h4>
-            </Link>
-        </Col>
-        <Col xs={3}>
-            <Link to="projects" smooth={true} duration= {500}>
-                <h4 className="navLink">Projects</h4>
-            </Link>
-        </Col>
-    </Row>
-</Container>
+    // Takes over functionality of {componentDidMount, componentDidUpdate}
+    useEffect(() => {
+        window.addEventListener('scroll', navOnScroll)
+        return function cleanup() {
+            window.removeEventListener('scroll', navOnScroll)
+        };
+    }, []);
 
-);
+    let navOnScroll = () => {
+        if (window.scrollY > 20) {
+            setScrolled(true)
+        } else {
+            setScrolled(false)
+        }
+    }
+
+    let links = [
+        {
+            name: 'Contact',
+            to: 'contact',
+            offset: -300,
+        },
+        {
+            name: "Projects",
+            to: "projects",
+            offset: -75,
+        }
+    ];
+
+    return (
+        
+    <Container fluid className={scrolled ? 'nav scroll' : 'nav'}>
+        <div className="nav-container">
+            <div className="home-nav">
+                <h4 className="navLink" onClick={scrollToTop}> Home </h4>
+            </div>
+            <div className="links">
+                {links.map(link => (
+                    <Link 
+                        to={link.to}
+                        offset={link.offset}
+                        smooth={true}
+                        duration={500}
+                    >
+                        <h4 className="navLink"> {link.name} </h4>
+                    </Link>
+                ))}
+            </div>
+        </div>
+            
+        
+
+    </Container>
+
+    );
 }
 
 export default NavBar;
